187ee967ddffa1ef582ee33d6b5ca362e5bc935c
조수아 <suacho0724@gmail.com>
2024-11-18 11:06:49 +0900

    ✨ Feat: add ai part

---
+++ deepfake_detector/ai/infer.py
@@ -0,0 +1,4 @@
+from furiosa.quantizer import load_model
+
+model_path = "./ENTROPY_ASYM.dfg"
+quantized_mdoel = load_model(model_path)
--- deepfake_detector/views.py
+++ deepfake_detector/views.py
@@ -1,3 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+from PIL import Image
+
+@csrf_exempt
+def inference(request):
+  if request.method == 'POST' and request.FILES.get('image'):
+    image_file = request.FILES['image']
+    image = Image.open(image_file)
+  return JsonResponse({'error': 'Invalid request'}, status=400)
